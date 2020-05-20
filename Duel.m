a829b1b450cd2448a4b65b019f6f28b16ec46c3c
hiGepi <sasasrb93@hotmail.fr>
2020-05-20 02:18:04 +0200

    Add files via upload

--- Duel.m
+++ Duel.m
@@ -2,7 +2,9 @@ function Char = Duel(Char)
 % On fait s'affronter tous les Chars entre eux
     for i = 1:100
         for j = 1:100
-            Char(1,i).fight_to_death(Char(1,j));
+            if i ~= j
+                Char(1,i).fight_to_death(Char(1,j));
+            end
         end
     end
 end
